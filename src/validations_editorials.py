47e262222110c5c37b032c9ea179cb12cd2aa523
María <117597145+MariaQuinFer@users.noreply.github.com>
2023-11-04 12:05:49 +0100

    Delete duplicate file

--- src/validations_editorials.py
+++
@@ -1,7 +0,0 @@
-# Valida el nombre de la editorial (si es un texto de entre 1 y 255 caracteres).
-def validate_name(name: str) -> bool:
-    return (len(name) > 0 and len(name) <= 255)
-
-
-def validate_id(id: str) -> bool:
-    return (id.isnumeric())
